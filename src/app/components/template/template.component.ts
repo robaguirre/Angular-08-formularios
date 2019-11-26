951929cd0f4839c3453521f1827eb60c93f0a208
Roberto Agirre <r.agirre@ibermatica.com>
2019-11-26 12:26:41 +0100

    Check y radio

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -15,8 +15,22 @@ export class TemplateComponent {
   usuario = {
     nombre: null,
     apellido: null,
-    correo: null
+    correo: null,
+    pais: '',
+    sexo: 'Hombre',
+    acepta: false
   };
+
+  paises = [{
+    codigo: 'CRI',
+    nombre: 'Costa Rica'
+  },
+  {
+    codigo: 'ESP',
+    nombre: 'España'
+  }];
+
+  sexos: string[] = ['Hombre', 'Mujer', 'Sin definir'];
 
   constructor() { }
 
